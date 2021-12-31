9af33c522a01b6755578f4804d62046a213c79a1
Michael Vo <zax@zaxu.xyz>
2021-12-31 16:50:56 +1100

    feat: Twitch Colours project

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,37 +1,31 @@
 <template lang="pug">
-  div
-    h1 Projects
+div
+  h1 Projects
 
-    section
-      .img
-        img(
-          src="/img/tracker.png"
-          alt="Project Showcase"
-        )
-      .description
-        h2
-          Link(href="https://tracker.zaxu.xyz") KH Rando Tracker
-        p A web-based tracker using #[strong Vue.js] and #[strong TypeScript] for "randomiser" mods for the Kingdom Hearts series video games, #[strong used by thousands of people around the world].
-        p Features a "Co-op" mode, synchronising clients using #[strong WebSockets] powered by a #[strong Python] server.
-        p Integrates with the #[Link(href="https://obsproject.com/") Open Broadcaster Software (OBS)] to embed a transparent overlay, which can be remotely controlled from a client through WebSockets.
-        //- p Helps people keep track of their progress with the items they've obtained and worlds they've visited/completed.
-        p Abstracts away a large part of the #[Link(href="https://jsmartee.github.io/kh2fm-hints-demo/info.html#logic") logic] involved in hints for people to easily use them to their advantage, by making as many deducations from the current information as possible.
-        //- p Only tracker for the randomiser that manages to convey as much information as it does in such a compact space while still being aesthetically pleasing.
-        p Supports mobile, allowing Twitch streamers to use their phone to control the on-stream tracker without unfocusing the game on their desktop.
-        
-        a.github(href="https://github.com/zaxutic/kh-rando-tracker") Source Code
+  section
+    .img
+      img(src="/img/tracker.png" alt="Project Showcase")
+    .description
+      h2
+        Link(href="https://tracker.zaxu.xyz") KH Rando Tracker
+      p A web-based tracker using #[strong Vue.js] and #[strong TypeScript] for "randomiser" mods for the Kingdom Hearts series video games, #[strong used by thousands of people around the world].
+      p Features a "Co-op" mode, synchronising clients using #[strong WebSockets] powered by a #[strong Python] server.
+      p Integrates with the #[Link(href="https://obsproject.com/") Open Broadcaster Software (OBS)] to embed a transparent overlay, which can be remotely controlled from a client through WebSockets.
+      p Abstracts away a large part of the #[Link(href="https://jsmartee.github.io/kh2fm-hints-demo/info.html#logic") logic] involved in hints for people to easily use them to their advantage, by making as many deducations from the current information as possible.
+      p Supports mobile, allowing Twitch streamers to use their phone to control the on-stream tracker without unfocusing the game on their desktop.
 
-    section
-      .img
-        img(
-          src="/img/colours.png"
-          alt="Project Showcase"
-        )
-      .description
-        h2
-          Link(href="https://colours.zaxu.xyz") Twitch Colours
-        
-        a.github(href="https://github.com/zaxutic/colours") Source Code
+      a.github(href="https://github.com/zaxutic/kh-rando-tracker") Source Code
+
+  section
+    .img
+      img(src="/img/colours.png" alt="Project Showcase")
+    .description
+      h2
+        Link(href="https://colours.zaxu.xyz") Twitch Colours
+      p A full-stack web application using #[strong Vue.js], #[strong Django], and #[strong SQLite], integrating with the Twitch OAuth 2.0 flow for user authentiaction.
+      p Uses #[strong Celery] and #[strong Redis] to asynchronously handle subroutines to connect to Twitch's IRC servers and run in the background to dynamically change the user's colour after every message.
+
+      a.github(href="https://github.com/zaxutic/colours") Source Code
 </template>
 
 <script>
@@ -76,14 +70,12 @@ a
     padding .5em .75em
     border-radius 2em
     background-color hsl(0, 0, 15%)
-    box-shadow 0 4px 6px -1px rgba(0, 0, 0, 0.1),
-      0 2px 4px -1px rgba(0, 0, 0, 0.06)
+    box-shadow 0 4px 6px -1px rgba(0, 0, 0, .1), 0 2px 4px -1px rgba(0, 0, 0, .06)
     transition background-color .2s, color .2s, box-shadow .2s
 
     &:hover
       background-color hsl(0, 0, 20%)
-      box-shadow 0 10px 15px -3px rgba(0, 0, 0, 0.1),
-        0 4px 6px -2px rgba(0, 0, 0, 0.05)
+      box-shadow 0 10px 15px -3px rgba(0, 0, 0, .1), 0 4px 6px -2px rgba(0, 0, 0, .05)
 
     &::before
       content ''
@@ -116,8 +108,7 @@ h2
 .img
   position relative
   border-radius 4px
-  box-shadow 0 20px 25px -5px rgba(0, 0, 0, 0.1),
-    0 10px 10px -5px rgba(0, 0, 0, 0.04)
+  box-shadow 0 20px 25px -5px rgba(0, 0, 0, .1), 0 10px 10px -5px rgba(0, 0, 0, .04)
   overflow hidden
 
   &::after
