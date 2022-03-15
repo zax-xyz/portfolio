86e30adb1f8632a005820808f93ee137dd37fcac
Michael Vo <zax@zaxu.xyz>
2022-03-15 11:16:54 +1100

    feat(about): remove "final"

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 Section(title="Hi there!")
-  p I'm Michael, a third (final) year student at the University of Sydney, studying a Bachelor of Computing, and majoring in Computer Science. While I am a university student, I started out as a self-taught developer, programming throughout high school and continuing to work on my own projects.
+  p I'm Michael, a third year student at the University of Sydney, studying a Bachelor of Computing, and majoring in Computer Science. While I am a university student, I started out as a self-taught developer, programming throughout high school and continuing to work on my own projects.
   p I've gone through a few different areas but my current focus is frontend web development, and creating beautiful and usable interfaces. I wish to bring as much value to others as possible, empowering people and making the world a better place where I can, while trying to improve myself at the same time and learn as much as I can.
   p I want to improve myself not just as a developer but as a person and grow emotionally and mentally.
   p I'm a supporter of Free &amp; Open Source Software (FOSS), Linux, and minimalism where appropriate — both in technology and in life in general.
