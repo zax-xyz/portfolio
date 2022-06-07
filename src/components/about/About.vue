6464c53056d46f8d77d60139fb4794dcfd675f2d
Michael Vo <zax@zaxu.xyz>
2022-06-07 14:11:12 +1000

    feat(about): add akuna

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -4,7 +4,7 @@ Section(title="Hi there!")
   p I've gone through a few different areas but my current focus is frontend web development, and creating beautiful and usable interfaces. I wish to bring as much value to others as possible, empowering people and making the world a better place where I can, while trying to improve myself at the same time and learn as much as I can.
   p I want to improve myself not just as a developer but as a person and grow emotionally and mentally.
   p I'm a supporter of Free &amp; Open Source Software (FOSS), Linux, and minimalism where appropriate — both in technology and in life in general.
-  p I recently worked at #[Link(href="https://www.canva.com/") Canva] over the 2021–2022 summer as a Frontend Software Engineer Intern, and I'm currently a member of the projects subcommitte in the #[Link(href="https://csesoc.unsw.edu.au/") Computer Science and Engineering Society (CSESoc)] at the University of New South Wales (UNSW).
+  p I'm going to be working at #[Link(href="https://akunacapital.com/") Akuna Capital] this upcoming summer as a Software Development Intern in the web team, and I recently worked at #[Link(href="https://www.canva.com/") Canva] over the 2021–2022 summer as a Frontend Software Engineer Intern. I'm also currently a member of the projects subcommitte in the #[Link(href="https://csesoc.unsw.edu.au/") Computer Science and Engineering Society (CSESoc)] at the University of New South Wales (UNSW), where I'm working in a team on the React frontend for a web-based #[Link(href="https://github.com/csesoc/chaos") hiring management system].
 </template>
 
 <script>
